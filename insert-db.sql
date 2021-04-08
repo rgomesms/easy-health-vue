dc5bdc56e819c3a09d63c9fd628de2e94f6d177d
Rodrigo Gomes Moreira da SIlva <rgomesms227@gmail.com>
2021-04-08 20:20:00 -0300

    Script Popular DB

---
+++ insert-db.sql
@@ -0,0 +1,35 @@
+
+insert into `Pessoa` values(1, 'Gabriel Ribeiro','gabrielribeiro@email.com','31999999991','80520260','Rua Tapajós','Bom Retiro','Curitiba','PR');
+insert into `Pessoa` values(2, 'Davi Emediato','daviemediato@email.com','31999999992','35162059','Rua Esmeralda','Iguaçu','Ipatinga','MG');
+insert into `Pessoa` values(3, 'Rafael Silverio','rafaelsilverio@email.com','31999999993','22710110','Rua Acre','Taquara','Rio de Janeiro','RJ');
+insert into `Pessoa` values(4, 'Iury Zanonni','iuryzanonni@email.com','31999999994','79640612','Rua Faveiro','Novo Oeste','Três Lagoas','MS');
+insert into `Pessoa`values(5, 'Rodrigo Gomes','rodrigogomes@email.com','31999999995','30421260','Rua Joaquim Nabuco','Nova Suíssa','Belo Horizonte','MG');
+insert into `Pessoa` values(6, 'Savio Saviola','saviosaviola@email.com','31999999996','30431198','Rua C','Grajaú','Belo Horizonte','MG');
+insert into `Pessoa` values(7, 'Lucca Miranda','luccamiranda@email.com','31999999997','30670230','Rua N','Vila Pinho Vale do Jatobá (Barreiro)','Belo Horizonte','MG');
+
+
+insert into `Funcionario` values(1,'2020-04-12','10000','40bd001563085fc35165329ea1ff5c5ecbdbbeef');
+insert into `Funcionario` values(2,'2020-05-15','11000','40bd001563085fc35165329ea1ff5c5ecbdbbeef');
+insert into `Funcionario` values(3,'2019-02-12','12000','40bd001563085fc35165329ea1ff5c5ecbdbbeef');
+insert into `Funcionario` values(7,'2019-03-12','10000','40bd001563085fc35165329ea1ff5c5ecbdbbeef');
+
+
+insert into `Paciente` values(4,'O','80','1,72');
+insert into `Paciente` values(5,'B','85','1,75');
+insert into `Paciente` values(6,'A','90','1,65');
+
+insert into `Medico` values(1,'Urologista','F000001');
+insert into `Medico` values(2,'Otorrino','F000002');
+insert into `Medico` values(7,'Otorrino','F000003');
+
+
+insert into `Agenda` values(4,'2021-04-08 14:00:00','Iury Zanonni','iuryzanonni@email.com','31999999994',1);
+insert into `Agenda` values(5,'2021-04-08 15:00:00','Rodrigo Gomes','rodrigogomes@email.com','31999999995',2);
+
+insert into `Enderecos` values('30421790','Beco Realino','Ventosa','Belo Horizonte','MG');
+insert into `Enderecos` values('31255056','Rua São Caetano','São Francisco','Belo Horizonte','MG');
+insert into `Enderecos` values('31530430','Rua Doutor Alberto Cambraia de Abreu','Santa Mônica','Belo Horizonte','MG');
+insert into `Enderecos` values('30421615','Beco Marilinha','Ventosa','Belo Horizonte','MG');
+insert into `Enderecos` values('30880210','Rua Homero','Glória','Belo Horizonte','MG');
+insert into `Enderecos` values('30626200','Rua Mauro José Brito','Brasil Industrial (Barreiro)','Belo Horizonte','MG');
+
