170e73d964c0d04163b11fd08c82cc25983f4d9d
Rodrigo Gomes Moreira da SIlva <rgomesms227@gmail.com>
2021-04-11 19:19:51 -0300

    wip - datepickercomponent

--- front/src/components/DatePickerComponent.vue
+++ front/src/components/DatePickerComponent.vue
@@ -1,23 +1,31 @@
-<template v-slot:activator="{ on, attrs }">
-    <v-text-field
-        v-model="date"
-        label="Picker without buttons"
-        append-icon="mdi-calendar"
-        readonly
-    ></v-text-field>
-    <!-- <v-text-field
-        v-model="date"
-        label="Picker without buttons"
-        prepend-icon="mdi-calendar"
-        readonly
-        v-bind="attrs"
-        v-on="on"
-    ></v-text-field> -->
+<template>
+    <v-menu
+        v-model="menu2"
+        :close-on-content-click="false"
+        :nudge-right="40"
+        transition="scale-transition"
+        offset-y
+        min-width="auto"
+    >
+        <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+                v-model="date"
+                label="Picker without buttons"
+                append-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+            ></v-text-field>
+        </template>
+        <v-date-picker v-model="date" @input="menu2 = false"></v-date-picker>
+    </v-menu>
 </template>
 
 <script>
+import { VMenu } from "vuetify/lib";
 export default {
     name: "DatePickerComponent",
+    components: { VMenu },
     data: () => ({
         date: new Date().toISOString().substr(0, 10),
         menu: false,
