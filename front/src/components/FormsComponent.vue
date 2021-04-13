152645c5031fd57539220e5ff508dafc3caa4db6
Rodrigo Gomes Moreira da SIlva <rgomesms227@gmail.com>
2021-04-13 04:02:59 -0300

    Adicionado validacao no input

--- front/src/components/FormsComponent.vue
+++ front/src/components/FormsComponent.vue
@@ -5,11 +5,11 @@
                 :is="field.is"
                 v-bind="field"
                 :key="field.label"
-                :rules="genericRules"
+                :rules="rules(field.required)"
                 v-on="field.on"
                 v-model="field.value"
                 @change="validate"
-                required
+                :required="field.required"
             />
         </template>
 
@@ -52,12 +52,12 @@ export default {
         genericRules: [(v) => !!v || "Campo obrigatório"],
     }),
     methods: {
-        validate(e) {
-            console.log(this.fields.cep.value, this.fields.cep);
-            console.log("Entrei na validação", e);
+        validate() {
+            // console.log("Entrei na validação", e);
             return this.$refs.form.validate();
         },
         submit() {
+            console.log("Submit aqui na quebrada", this.$refs.form.validate());
             if (this.$refs.form.validate()) {
                 this.buttons.submit.on.click();
                 this.$refs.form.reset();
@@ -73,8 +73,10 @@ export default {
         DatePickerComponent: DatePickerComponent,
     },
     computed: {
-        currentTabComponent: function() {
-            return "v-text-field";
+        rules: function() {
+            return function(required) {
+                return required ? [(v) => !!v || "Campo obrigatório"] : [];
+            };
         },
     },
 };
